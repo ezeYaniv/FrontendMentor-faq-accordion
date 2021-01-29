0997a73b7e02b1318583c3bde0406e5c9b23f4b5
ezeYaniv <brener.yaniv@gmail.com>
2021-01-29 12:48:59 -0700

    finish mobile styles. start desktop styles

---
+++ accordion.js
@@ -0,0 +1,35 @@
+//THIS CODE ADDS EVENT LISTENERS FOR CLICK
+$(document).ready(function () {
+  let row = document.getElementsByClassName("list__heading-row");
+  row = Array.from(row);
+  row.forEach((element) => {
+    element.addEventListener("click", showText);
+  });
+
+
+//   //THIS CODE STYLES ALL THE INNER STUFF
+//   for (let i = 0; i < row.length; i++) {
+//     row[i].classList.remove("active-row");
+//     // row.getElementsByClassName("list__text--main")[i].classList.remove("active-text-bold");
+//   }
+});
+
+//THIS CODE CREATES A FUNCTION TO ADD AND REMOVE CLASS NAMES
+function showText() {
+  if (this.classList.contains("active-row")) {
+    this.classList.remove("active-row");
+    this.getElementsByClassName("list__text--main")[0].classList.remove("active-text-bold");
+    this.getElementsByClassName("list__dropdown")[0].classList.remove("active-dropdown");
+    $(this).siblings(".list__text--collapsible")[0].style.display = "none";
+} else {
+      //Need some code here to remove from all the others
+
+      //ADDS ALL NECESSARY CLASSES TO THESE CHILDREN
+    this.classList.add("active-row");
+    this.getElementsByClassName("list__text--main")[0].classList.add("active-text-bold");
+    this.getElementsByClassName("list__dropdown")[0].classList.add("active-dropdown");
+    $(this).siblings(".list__text--collapsible")[0].style.display = "inline-block";
+  }
+}
+
+
