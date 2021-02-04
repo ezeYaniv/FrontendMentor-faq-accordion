96632648b2c00df3d061a4233d6d451389573d41
ezeYaniv <brener.yaniv@gmail.com>
2021-02-04 16:25:46 -0700

    add is-active class and simplify logic for active styles

--- accordion.js
+++ accordion.js
@@ -12,15 +12,11 @@ function showText() {
     //THIS CODE REMOVES ALL EXISTING CLASSES WHEN YOU CLICK AN ALREADY-SELECTED ROW
   if (this.classList.contains("active-row")) {
     this.classList.remove("active-row");
-    this.getElementsByClassName("list__text--main")[0].classList.remove("active-text-bold");
-    this.getElementsByClassName("list__dropdown")[0].classList.remove("active-dropdown");
     $(this).siblings(".list__text--collapsible")[0].style.display = "none";
 } else {
 
       //THIS CODE ADDS ALL NECESSARY CLASSES TO THE ROW'S CHILDREN
     this.classList.add("active-row");
-    this.getElementsByClassName("list__text--main")[0].classList.add("active-text-bold");
-    this.getElementsByClassName("list__dropdown")[0].classList.add("active-dropdown");
     $(this).siblings(".list__text--collapsible")[0].style.display = "inline-block";
   }
 }
